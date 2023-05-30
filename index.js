73d5f85fabdb1cd98746a91ebbf3c23be4c3e009
abel <abelw34@gmail.com>
2023-05-30 19:50:41 +0300

    back end

--- index.js
+++ index.js
@@ -34,59 +34,69 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
 
-io.on("connection", (socket) => {
-  //connect to the chat
-  socket.on("connectAgent", ({ username, room }) => {
-    const user = userJoin(socket.id, username, room);
-
-    socket.join(user.room);
-
-    socket.emit(
-      "message",
-      fomartMessage(
-        "Agent",
-        " Hello, welcome to Super Awesome Co. How may I help you?"
-      )
-    );
-
-    socket.broadcast
-      .to(user.room)
-      .emit("message", fomartMessage("Agent", ` ${user.username} joined`));
-
-    //send users info
-    io.to(user.room).emit("roomUsers", {
-      room: user.room,
-      user: getRoomUser(user.room),
-    });
-  });
-
-  //user chats
-  socket.on("conversation", (msg) => {
-    const user = getCurrentUser(socket.id);
-    io.to(user.room).emit("message", fomartMessage(user.username, msg));
-  });
-
-  //user disconects from the chat
-  socket.on("disconnect", () => {
-    const user = userLeaves(socket.id);
-    if (user) {
-      io.to(user.room).emit(
-        "message",
-        fomartMessage("Agent", `${user.username} left`)
-      );
-
-      io.to(user.room).emit("roomUsers", {
-        room: user.room,
-        user: getRoomUser(user.room),
-      });
-    }
-  });
-});
-
-mongoose.set("strictQuery", true);
-
 connectDB(process.env.MONGO_URI)
   .then(() => {
+    io.on("connection", (socket) => {
+      //connect to the chat
+      socket.on("connectAgent", ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room);
+
+        socket.emit(
+          "message",
+          fomartMessage(
+            "Agent",
+            " Hello, welcome to Super Awesome Co. How may I help you?"
+          )
+        );
+
+        socket.broadcast
+          .to(user.room)
+          .emit("message", fomartMessage("Agent", ` ${user.username} joined`));
+
+        //send users info
+        io.to(user.room).emit("roomUsers", {
+          room: user.room,
+          user: getRoomUser(user.room),
+        });
+      });
+
+      //user chats
+      socket.on("conversation", (msg) => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit("message", fomartMessage(user.username, msg));
+
+        const newChat = new Chat({
+          name: user.username,
+          content: msg,
+          room: user.room,
+        });
+
+        newChat.save();
+      });
+
+      //user disconects from the chat
+      socket.on("disconnect", () => {
+        const user = userLeaves(socket.id);
+        if (user) {
+          io.to(user.room).emit(
+            "message",
+            fomartMessage("Agent", `${user.username} left`)
+          );
+
+          io.to(user.room).emit("roomUsers", {
+            room: user.room,
+            user: getRoomUser(user.room),
+          });
+        }
+      });
+    });
+
+    mongoose.set("strictQuery", true);
+
     server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   })
   .catch((err) => console.log(err));
+
+//when sockets are running can you perfom asyncronous requests
